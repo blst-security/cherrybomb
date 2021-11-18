18b126020bf15e515d09b41034bb39e406a22461
GuyL99 <guy123levinger@gmail.com>
2021-11-18 15:18:23 +0200

    added better formatting

--- digest/src/req_res.rs
+++ digest/src/req_res.rs
@@ -1,4 +1,5 @@
 use super::*;
+use std::fmt;
 #[derive(Debug,Clone,Serialize,Deserialize,Default,PartialEq,Eq)]
 pub struct ReqRes{
     pub req_headers:HashMap<String,String>,
@@ -9,4 +10,9 @@ pub struct ReqRes{
     pub req_payload:String,
     pub res_payload:String,
     pub req_query:String,
+}
+impl std::fmt::Display for ReqRes{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "method:{:?}\tpath:{}\tquery:{}\nrequest headers:{:?}\nrequest_payload:{}\nstatus:{}\nresponse_headers:{:?}\nresponse_payload:{}", self.method, self.path,self.req_query,self.req_headers,self.req_payload,self.status,self.res_headers,self.res_payload)
+    }
 }
