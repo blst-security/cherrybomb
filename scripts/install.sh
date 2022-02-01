f27aaf21dea8a5f1ccc581957dfa89bf476c11e4
Guy Levinger <guy123levinger@gmail.com>
2022-02-01 17:58:02 +0200

    Update install.sh
    Now creates a .cherrybomb folder in  home for future use

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,9 +6,9 @@ case $unamestr in
   "SunOs") echo "\033[31;1m SOLARIS is not yet supported, contact us to get support status or open a github issue\033[0m" ; exit 0;;
   "Darwin")  platform="mac" ;;
   "Linux")   platform="linux" ;;
-  "FreeBSD")     echo "\033[31;1m BSD is not yet supported, contact us to get support status or open a github issue\033[0m" ; exit 0;;
-  "WindowsNT")    echo "\033[31;1m WINDOWS is not yet supported, contact us to get support status or open a github issue\033[0m" ; exit 0;;
-  *)        echo "\033[31;1m unknown: $OSTYPE is not yet supported, contact us to get support status or open a github issue\033[0m" ; exit 0;;
+  "FreeBSD")     echo "\033[31;1m BSD is not yet supported, contact us to get support status or open a github issue\033[0m" ; exit 0;; 
+  "WindowsNT")    echo "\033[31;1m WINDOWS is not yet supported, contact us to get support status or open a github issue\033[0m" ; exit 0;; 
+  *)        echo "\033[31;1m unknown: $OSTYPE is not yet supported, contact us to get support status or open a github issue\033[0m" ; exit 0;;  
 esac
 echo "\033[32;1m DETECTED OS - ${platform}\033[0m";
 filename="cherrybomb_${platform}"
@@ -21,6 +21,7 @@ pre=$(echo "$presigned" | sed -e 's/^"//' -e 's/"$//');
 c=$(curl -s ${pre} -o cherrybomb);
 echo "\033[32;1m DONE DOWNLOADING\033[0m"
 echo "\033[34;1m INSTALLING\033[0m"
+mkdir ~/.cherrybomb 2> /dev/null
 chmod +x cherrybomb;
 sudo mv cherrybomb /usr/local/bin/
 echo "\033[32;1m DONE INSTALLING RUN cherrybomb to test\033[0m"
