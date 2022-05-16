47e50ac68676e99906b5d671895c8c43731b24cb
Guy Levinger <guy123levinger@gmail.com>
2022-05-16 17:40:57 +0300

    Update main.rs

--- swagger/src/main.rs
+++ swagger/src/main.rs
@@ -2,7 +2,7 @@ use swagger::*;
 
 #[tokio::main]
 async fn main() {
-    let f_name = "/home/raz/Downloads/juice.yml";
+    let f_name = "swagger3.json";
     //let _swagger_str:Swagger = serde_json::from_str(&std::fs::read_to_string(f_name).unwrap()).unwrap();
     //let f_names = ["swagger2.json","swagger3.json","swagger4.json","swagger5.json","swagger6.json","swagger7.json"];
     //for f_name in f_names{
