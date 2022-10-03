1ce29b474a63096fecd814313b6423b6a0df9739
Nathan <nathansit20@gmail.com>
2022-10-03 20:07:06 +0300

    Added  delete method permission

--- swagger/src/main.rs
+++ swagger/src/main.rs
@@ -2,24 +2,24 @@ use swagger::*;
 
 #[tokio::main]
 async fn main() {
-    let f_name = "/home/nathan/Documents/POC/Cherry/Testing/openapi.json";
+    let f_name = "/home/nathan/Documents/POC/Cherry/Testing/crapi.json";
     let swagger_value: serde_json::Value =
         serde_json::from_str(&std::fs::read_to_string(f_name).unwrap()).unwrap();
 
     let mut a = PassiveSwaggerScan::<OAS3_1>::new(swagger_value.clone()).unwrap();
     a.run(PassiveScanType::Full);
-    a.print(1);
+    //a.print(1);
     //  a.print(0);
 
     let a = ParamTable::new::<OAS3_1>(&swagger_value);
-    a.print();
+    //a.print();
 
     let a = EpTable::new::<OAS3_1>(&swagger_value);
-    a.print();
+    //a.print();
 
     let mut a = ActiveScan::<OAS3_1>::new(swagger_value).unwrap();
     use futures::executor;
     executor::block_on(a.run(ActiveScanType::Full, &Authorization::None));
     a.print(1);
-    a.print(0);
+   // a.print(0);
 }
--- swagger/src/scan/active/additional_checks.rs
+++ swagger/src/scan/active/additional_checks.rs
@@ -464,9 +464,102 @@ impl<T: OAS + Serialize> ActiveScan<T> {
         }
         ret_val
     }
+    pub async fn check_broken_object(&self, auth: &Authorization) -> CheckRetVal {
+        let mut ret_val = CheckRetVal::default();
+        let mut vec_param: Vec<RequestParameter> = Vec::new();
+        let server = &self.oas.servers();
+       // let mut param_object;
+       let mut flag = false;
+        for (path, item) in &self.oas.get_paths() {
+            for (m, op) in item.get_ops().iter().filter(|(m, _)| m == &Method::GET) {
+                for i in op.params() {
+                    if i.inner(&self.oas_value).param_in.to_string().to_lowercase()
+                        == "path".to_string()|| i.inner(&self.oas_value).param_in.to_string().to_lowercase() == "query"{
+                            flag = true;
+                            
+                        }
+                   
+                   // if i.inner(&self.oas_value).name.to_lowercase().contains(&"id".to_string())
+                    let mut param_obj ;
+                    if flag{
+                     //   param_object = i.inner(&self.oas_value).name;
+                     let resp =  op.responses();
+                     let data_resp = resp.get(&"200".to_string());
+                     if let Some(value)= data_resp {
+                        param_obj = value.inner(&self.oas_value).content.unwrap().into_values();
+                        for i in param_obj {
+                           println!("SHEMMMMMMAAAAA {:?}", i.schema.unwrap().inner(&self.oas_value).required);
+                        }
+                       // println!("PARAMMMM. {:?}", &param_obj);clear
+
+
+                     }
+                        if let Some(types) = i
+                            .inner(&self.oas_value)
+                            .schema()
+                            .inner(&self.oas_value)
+                            .schema_type
+                        {
+                            let mut value_to_send="2".to_string();
+                            let mut var_int:i32= 2;
+                            if types == "integer".to_string() {
+                                if let Some(val) = i
+                                    .inner(&self.oas_value)
+                                    .examples
+                                {
+                                    if let Some((_ex, val)) = val.into_iter().next() {
+                                        value_to_send = val.value.to_string();
+                                        var_int = value_to_send.parse::<i32>().unwrap();
+                                            
+                                    }
+                                    for n in var_int-1..var_int+1 {
+                                    let param_to_send: RequestParameter = RequestParameter {
+                                        name: i.inner(&self.oas_value).name.to_string(),
+                                        value: n.to_string(),
+                                        dm: QuePay::Query,
+                                    };
+                                    vec_param.push(param_to_send);
+                                    let req = AttackRequest::builder()
+                                        .uri(server,path)
+                                        .method(*m)
+                                        .auth(auth.clone())
+                                        .build();
+                                    if let Ok(res) = req.send_request(self.verbosity > 0).await {
+                                        //logging request/response/description
+                                        ret_val.1.push(
+                                            &req,
+                                            &res,
+                                            "Testing for BOLA".to_string(),
+                                        );
+                                        ret_val.0.push((
+                                            ResponseData {
+                                                location: path.clone(),
+                                                alert_text: format!(
+                                    "The server: {} is not secure against https downgrade",
+                                    &path
+                                ),
+                                                serverity: Level::Medium,
+                                            },
+                                            res.clone(),
+                                        ));
+                                    } else {
+                                        println!("REQUEST FAILED");
+                                    }
+                                }
+                                }
+                            }
+                        }
+                    
+                    }
+                  
+                }
+            }
+        }
+        ret_val
+    }
 }
 
-const LIST_METHOD: [Method; 3] = [Method::GET, Method::POST, Method::PUT];
+const LIST_METHOD: [Method; 3] = [Method::GET, Method::POST, Method::PUT, Method::DELETE];
 
 const LIST_PARAM: [&str; 85] = [
     "photoUrls",
