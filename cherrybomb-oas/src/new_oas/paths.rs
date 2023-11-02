71b5891aa44f476720b3bf7aa05f639dde27ce76
razmag <46993372+RazMag@users.noreply.github.com>
2023-11-02 16:01:53 +0000

    working on "Path"

--- cherrybomb-oas/src/new_oas/paths.rs
+++ cherrybomb-oas/src/new_oas/paths.rs
@@ -9,3 +9,31 @@ pub struct Paths {
     #[serde(flatten)]
     pub extensions: HashMap<String, Value>,
 }
+
+#[derive(Debug, Clone, Serialize, Deserialize, Default)]
+pub struct PathItem {
+    pub item_ref: Option<RelReference>,
+    pub summary: Option<String>,
+    pub description: Option<String>,
+    pub get: Option<Operation>,
+    pub put: Option<Operation>,
+    pub post: Option<Operation>,
+    pub delete: Option<Operation>,
+    pub options: Option<Operation>,
+    pub head: Option<Operation>,
+    pub patch: Option<Operation>,
+    pub trace: Option<Operation>,
+    pub servers: Option<Vec<crate::new_oas::server::Server>>,
+    pub parameters: Option<Vec<ParamRef>>,
+}
+
+enum ParamRef {
+    Ref(RelReference),
+    Param(Parameter),
+}
+
+pub struct Operation {
+
+}
+
+
