8b98d81fc6853b0f5a40a6841d57e4e208448470
razmag <46993372+RazMag@users.noreply.github.com>
2023-11-06 12:46:00 +0000

    More work on "Paths"

--- cherrybomb-oas/src/new_oas/paths.rs
+++ cherrybomb-oas/src/new_oas/paths.rs
@@ -12,7 +12,6 @@ pub struct Paths {
 
 #[derive(Debug, Clone, Serialize, Deserialize, Default)]
 pub struct PathItem {
-    pub item_ref: Option<RelReference>,
     pub summary: Option<String>,
     pub description: Option<String>,
     pub get: Option<Operation>,
@@ -24,16 +23,65 @@ pub struct PathItem {
     pub patch: Option<Operation>,
     pub trace: Option<Operation>,
     pub servers: Option<Vec<crate::new_oas::server::Server>>,
-    pub parameters: Option<Vec<ParamRef>>,
 }
 
-enum ParamRef {
-    Ref(RelReference),
-    Param(Parameter),
-}
-
+#[derive(Debug, Clone, Serialize, Deserialize, Default, PartialEq)]
 pub struct Operation {
-
+    pub tags: Option<Vec<String>>,
+    pub summary: Option<String>,
+    pub description: Option<String>,
+    #[serde(rename = "externalDocs")]
+    pub external_docs: Option<ExternalDocs>,
+    #[serde(rename = "operationId")]
+    pub operation_id: Option<String>,
+    pub parameters: Option<Vec<RelRef>>,
+    #[serde(rename = "requestBody")]
+    pub request_body: Option<RelRef>,
+    pub responses: Option<Responses>,
+    pub callbacks: Option<HashMap<String, RelRef>>,
+    pub deprecated: Option<bool>,
+    pub security: Option<Vec<Security>>,
+    pub servers: Option<Vec<crate::new_oas::server::Server>>,
 }
 
+#[derive(Debug, Clone, Serialize, Deserialize, Default, PartialEq)]
+pub struct ExternalDocs {
+    pub description: Option<String>,
+    pub url: String,
+}
 
+#[derive(Debug, Clone, Serialize, Deserialize, Default, PartialEq)]
+pub struct Parameter{
+    pub name: String,
+    #[serde(rename = "in")]
+    pub in_: String,
+    pub description: Option<String>,
+    pub required: Option<bool>,
+    pub deprecated: Option<bool>,
+    #[serde(rename = "allowEmptyValue")]
+    pub allow_empty_value: Option<bool>,
+    pub style: Option<String>,
+    pub explode: Option<bool>,
+    #[serde(rename = "allowReserved")]
+    pub allow_reserved: Option<bool>,
+    pub schema: Option<Schema>,
+    pub example: Option<Value>,
+    pub examples: Option<HashMap<String, RelRef>>,
+    pub content: Option<HashMap<String, MediaType>>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, Default, PartialEq)]
+pub struct Responses {
+    #[serde(rename = "default")]
+    pub default: Option<RelRef>,
+    pub responses: Option<HashMap<String, RelRef>>,
+    pub extensions: Option<HashMap<String, Value>>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, Default, PartialEq)]
+pub struct Response {
+    pub description: String,
+    pub headers: Option<HashMap<String, RelRef>>,
+    pub content: Option<HashMap<String, MediaType>>,
+    pub links: Option<HashMap<String, RelRef>>,
+}
